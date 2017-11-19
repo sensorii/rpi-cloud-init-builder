719b8f9c95e076d61e716b939d70796e0b7283e8
Xendarboh Sensorii <sensorii@users.noreply.github.com>
2017-11-19 23:24:53 +0000

    Run tools with python2, move git config

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,22 +7,24 @@ git clone https://git.launchpad.net/cloud-init /tmp/cloud-init
 cd /tmp/cloud-init
 git checkout -b ${CLOUD_INIT_VERSION}-rpi ${CLOUD_INIT_VERSION}
 
+# configure git user
+# all mods and new files must be committed to be included without error
+git config user.email "you@example.com"
+git config user.name "Your Name"
+
 # apply small patch to fix failing lib imports
-# (git commit is required to avoid error about uncomitted changes)
 sed -i -e \
   's/from requests.packages.urllib3/from urllib3/' \
   cloudinit/sources/DataSourceScaleway.py
-git config user.email "you@example.com"
-git config user.name "Your Name"
 git commit -am'Fix urllib3 imports'
 
 # install dependencies
 # (this is extracted from `packages/bddeb` script)
-tools/read-dependencies \
+python tools/read-dependencies \
   --distro debian \
   --python-version 2 \
   --install
-tools/read-dependencies \
+python tools/read-dependencies \
   --distro debian \
   --python-version 2 \
   --system-pkg-names \
